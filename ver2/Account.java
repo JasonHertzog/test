3e67c0d3763064be63aeb519b92e240f031d796f
Jason Hertzog <86388948+JasonHertzog@users.noreply.github.com>
2022-04-27 16:48:00 -0400

    Added new comment
    Moved new comment above previous comment to follow lab more closely.

--- ver2/Account.java
+++ ver2/Account.java
@@ -1,8 +1,9 @@
 package ver2;
 
 public class Account {
+	// New Comment
 	// Instance variable, only available inside this class.
-	private double balance; // New Comment
+	private double balance; 
 	private String name;
 	
 	// Constructor
